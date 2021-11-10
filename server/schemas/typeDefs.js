4dab5a2c501337d27bdf32bc23b4d93a63533590
Ying Li <lliying1213@gmail.com>
2021-11-09 22:24:53 -0500

    Update typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,31 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  # set up types
+  # set up types from models
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    savedBooks: [Book]!
+  }
+
+  type Book {
+    _id: ID!
+    authors: String
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+
+  # Auth
+
+  # Query
+
+  # Mutation
 `;
 
 module.exports = typeDefs;
