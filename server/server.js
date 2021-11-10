f46d339dd0b0478645fb22efc954c44bc35e1c9b
Ying Li <lliying1213@gmail.com>
2021-11-10 17:15:32 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.use(routes);
+// app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
