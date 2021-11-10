ea5a0afc23d256dc4a15f9b7bbf7846fec0aa786
Ying Li <lliying1213@gmail.com>
2021-11-10 17:14:23 -0500

    Update typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,10 +4,11 @@ const typeDefs = gql`
   # set up types from models
 
   type User {
-    _id: ID
+    _id: ID!
     username: String
     email: String
     password: String
+    bookCount: Int
     savedBooks: [Book]!
   }
 
@@ -22,10 +23,23 @@ const typeDefs = gql`
   }
 
   # Auth
+  type Auth {
+    token: ID!
+    user: User
+  }
 
   # Query
+  type Query {
+    me: User
+  }
 
   # Mutation
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(newBook: InputBook!): User
+    removeBook(bookId: ID!): User
+  }
 `;
 
 module.exports = typeDefs;
