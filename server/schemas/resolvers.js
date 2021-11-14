b0d0818f9e1abf7fad86145998da7096cd33cee2
Ying Li <lliying1213@gmail.com>
2021-11-13 23:30:28 -0500

    Fixed server side bug on sign-up

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -9,7 +9,7 @@ const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
-        const userData = await User.findOne({ _id: context.user._id }).select('-_v -password');
+        const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
         return userData;
       }
       throw new AuthenticationError('You are not logged in!');
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -25,7 +25,7 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      // return res.status(400).json({ message: 'invalid token!' });
     }
 
     // return updated request
